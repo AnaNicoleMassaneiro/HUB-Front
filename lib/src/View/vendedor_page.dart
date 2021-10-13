4afe4c3ec68eec49c02a557b253b51781cf084a9
Ana Nicole <ananicole@MacBook-Pro-de-Ana.local>
2021-10-13 09:33:46 -0300

    tela vendedor acessando mapa

--- lib/src/View/vendedor_page.dart
+++ lib/src/View/vendedor_page.dart
@@ -1,13 +1,9 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
-import '../Widget/bezier_container.dart';
+import 'package:location/location.dart';
 
 class VendedorPage extends StatefulWidget {
-  VendedorPage({Key? key, required this.title}) : super(key: key);
+  const VendedorPage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
@@ -17,25 +13,31 @@ class VendedorPage extends StatefulWidget {
 class _VendedorPageState extends State<VendedorPage> {
   final TextEditingController user = TextEditingController();
   final TextEditingController senha = TextEditingController();
+  late GoogleMapController _controller;
+  final Location _location = Location();
+  final LatLng _initialcameraposition = const LatLng(-25.4340196, -49.2588484);
+  var location = Location();
+  late Map<String, double> userLocation;
+  late LocationData minhaLocalizacao;
   int _indiceAtual = 0;
-  Completer<GoogleMapController> _controller = Completer();
-  static const LatLng _center = const LatLng(45.521563, -122.677433);
-
-  void _onMapCreated(GoogleMapController controller) {
-    _controller.complete(controller);
-  }
 
   @override
   Widget build(BuildContext context) {
-    final height = MediaQuery.of(context).size.height;
-
     return Scaffold(
-        appBar: AppBar(title: const Text('ENTRAMOS NO VENDEDOR')),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
+        appBar: AppBar(title: const Text('Bem Vindo')),
+        body: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Stack(
+            children: [
+              GoogleMap(
+                initialCameraPosition:
+                    CameraPosition(target: _initialcameraposition),
+                mapType: MapType.normal,
+                onMapCreated: _onMapCreated,
+                myLocationEnabled: true,
+              ),
+            ],
           ),
         ),
         bottomNavigationBar: BottomNavigationBar(
@@ -45,7 +47,8 @@ class _VendedorPageState extends State<VendedorPage> {
             BottomNavigationBarItem(
                 icon: Icon(Icons.person), title: Text("localizacao")),
             BottomNavigationBarItem(
-                icon: Icon(Icons.shopping_basket), title: Text("Meus pedidos")),
+                icon: Icon(Icons.shopping_basket),
+                title: Text("Meus produtos")),
             BottomNavigationBarItem(
                 icon: Icon(Icons.favorite), title: Text("Favoritos")),
           ],
@@ -55,6 +58,28 @@ class _VendedorPageState extends State<VendedorPage> {
   void onTabTapped(int index) {
     setState(() {
       _indiceAtual = index;
+    });
+  }
+
+  void _onMapCreated(GoogleMapController _cntlr) {
+    _controller = _cntlr;
+    _location.onLocationChanged.listen((l) {
+      pegarLocalizacao();
+
+      _controller.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+              target: LatLng(minhaLocalizacao.latitude ?? -25.4340196,
+                  minhaLocalizacao.longitude ?? -49.2588484),
+              zoom: 15),
+        ),
+      );
+    });
+  }
+
+  void pegarLocalizacao() async {
+    setState(() async {
+      minhaLocalizacao = await location.getLocation();
     });
   }
 }
