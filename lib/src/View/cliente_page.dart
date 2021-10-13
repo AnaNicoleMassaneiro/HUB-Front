0e88895f9b636abade7d026f0ec401574befd51c
Ana Nicole <ananicole@MacBook-Pro-de-Ana.local>
2021-10-13 12:47:46 -0300

    componentizando mapa do cliente

--- lib/src/View/cliente_page.dart
+++ lib/src/View/cliente_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
+import 'Components/mapa.dart';
+
 class ClientePage extends StatefulWidget {
   const ClientePage({Key? key, required this.title}) : super(key: key);
   final String title;
@@ -13,9 +15,6 @@ class ClientePage extends StatefulWidget {
 class _ClientePageState extends State<ClientePage> {
   final TextEditingController user = TextEditingController();
   final TextEditingController senha = TextEditingController();
-  late GoogleMapController _controller;
-  final Location _location = Location();
-  final LatLng _initialcameraposition = const LatLng(-25.4340196, -49.2588484);
   var location = Location();
   late Map<String, double> userLocation;
   late LocationData minhaLocalizacao;
@@ -24,43 +23,7 @@ class _ClientePageState extends State<ClientePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('ENTRAMOS NO CIENTE')),
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: Stack(
-          children: [
-            GoogleMap(
-              initialCameraPosition:
-                  CameraPosition(target: _initialcameraposition),
-              mapType: MapType.normal,
-              onMapCreated: _onMapCreated,
-              myLocationEnabled: true,
-            ),
-          ],
-        ),
-      ),
+      body:  mapaComponent(this.context),
     );
-  }
-
-  void _onMapCreated(GoogleMapController _cntlr) {
-    _controller = _cntlr;
-    _location.onLocationChanged.listen((l) {
-      pegarLocalizacao();
-
-      _controller.animateCamera(
-        CameraUpdate.newCameraPosition(
-          CameraPosition(
-              target: LatLng(minhaLocalizacao.latitude ?? -25.4340196,
-                  minhaLocalizacao.longitude ?? -49.2588484),
-              zoom: 15),
-        ),
-      );
-    });
-  }
-
-  void pegarLocalizacao() async {
-    setState(() async {
-      minhaLocalizacao = await location.getLocation();
-    });
   }
 }
