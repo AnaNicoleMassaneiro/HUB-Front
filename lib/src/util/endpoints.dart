a3d023b2f52469369f0c23862efbce4488c8bbbb
sauxthem <matt.rock4321@hotmail.com>
2021-11-23 20:37:32 -0300

    Refactor Endpoint class name Add rating endpoints

--- lib/src/util/endpoints.dart
+++ lib/src/util/endpoints.dart
@@ -1,6 +1,6 @@
-class ReservaCreateEndpoints {
-  //http://192.168.0.148:4000
-  static const host = "http://192.168.0.148:4000/";
+class Endpoints {
+  //static const host = "http://192.168.1.2:5000/"; // ENDPOINT LOCAL MATHEUS
+  static const host = "http://192.168.0.148:4000"; // ENDPOINT LOCAL ANA
   static const create = host + "api/User/create";
   static const autenticate = host + "api/User/authenticate";
   static const searchProdutoPorVendedor =
@@ -18,4 +18,8 @@ class ReservaCreateEndpoints {
   static const getReservationBySeller = host + "api/reserva/getBySeller/";
   static const buscaPorLocalizacao = host + "api/vendedor/buscarPorLocalizacao";
   static const buscarUserPorId = host + "api/User/buscarUserPorId/";
+  static const insertRating = host + "api/avaliacao/insert";
+  static const getCustomeratings = host + "api/avaliacao/cliente/";
+  static const getSellerRatings = host + "api/avaliacao/vendedor/";
+  static const getProductRatings = host + "api/avaliacao/produto/";
 }
