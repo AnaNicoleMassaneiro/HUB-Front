7d0119c82e50c59f7d81d3d2fb32adc4df7b14cd
Ana Nicole <ananicole@MacBook-Pro-de-Ana.local>
2021-12-15 17:07:39 -0300

    FEAT - loading botao de login

--- lib/src/Api/api_login.dart
+++ lib/src/Api/api_login.dart
@@ -5,12 +5,16 @@ import 'package:http/http.dart' as http;
 // ignore: camel_case_types
 class apiLogin {
   Future<http.Response> login(String usuario, String senha) async {
-    final response = await http.post(Uri.parse(Endpoints.autenticate),
-        headers: <String, String>{
-          'Content-Type': "application/json; charset=UTF-8",
-        },
-        body: jsonEncode(<String, String>{'usuario': usuario, 'senha': senha}));
+    try {
+      final response = await http.post(Uri.parse(Endpoints.autenticate),
+          headers: <String, String>{
+            'Content-Type': "application/json; charset=UTF-8",
+          },
+          body: jsonEncode(<String, String>{'usuario': usuario, 'senha': senha}));
 
-    return response;
+        return response;
+    } catch (e) {
+      throw Exception(e);
+    }
   }
 }
