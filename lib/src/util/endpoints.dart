f93bfd177a559cc4d38e6d684119e8523e811a02
sauxthem <matt.rock4321@hotmail.com>
2021-12-14 21:09:13 -0300

    Refactor Endpoints.dart Add new endpoint Add "/api" prefix to host instead of adding to all endpoints

--- lib/src/util/endpoints.dart
+++ lib/src/util/endpoints.dart
@@ -1,51 +1,54 @@
 class Endpoints {
-  static const host = "https://hubufpr.herokuapp.com/"; // ENDPOINT HEROKU
-  //static const host = "http://192.168.1.2:5000/"; // ENDPOINT LOCAL
+  //static const host = "https://hubufpr.herokuapp.com/api/"; // ENDPOINT HEROKU
+  static const host = "http://192.168.1.11:5000/api/"; // ENDPOINT LOCAL
 
   // USER
-  static const create = host + "api/User/create";
-  static const autenticate = host + "api/User/authenticate";
-  static const updateLocation = host + "api/User/atualizarLocalizacao/";
-  static const buscarUserPorId = host + "api/User/buscarUserPorId/";
-  static const updateUserName = host + "api/User/updateUser/";
-  static const atualizarSenha = host + "api/User/atualizarSenha/";
+  static const create = host + "User/create";
+  static const autenticate = host + "User/authenticate";
+  static const updateLocation = host + "User/atualizarLocalizacao/";
+  static const buscarUserPorId = host + "User/buscarUserPorId/";
+  static const updateUserName = host + "User/updateUser/";
+  static const atualizarSenha = host + "User/atualizarSenha/";
 
   // VENDEDOR
-  static const buscarTodosVendedores = host + "api/vendedor/buscarTodos";
-  static const buscarVendedorPorId = host + "api/vendedor/buscarPorId/";
-  static const buscaPorLocalizacao = host + "api/vendedor/buscarPorLocalizacao";
-  static const atualizarStatusVendedor = host + "api/vendedor/atualizarStatus";
+  static const buscarTodosVendedores = host + "vendedor/buscarTodos";
+  static const buscarVendedorPorId = host + "vendedor/buscarPorId/";
+  static const buscaPorLocalizacao = host + "vendedor/buscarPorLocalizacao";
+  static const atualizarStatusVendedor = host + "vendedor/atualizarStatus";
 
   // VENDEDOR FAVORITO
-  static const addToFavorites = host + "api/vendedor/favoritos/adicionar";
-  static const removeFromFavorites = host + "api/vendedor/favoritos/remover";
-  static const getFavorites = host + "api/vendedor/favoritos/buscar/";
-  static const isFavorite = host + "api/vendedor/favoritos/isFavorite";
+  static const addToFavorites = host + "vendedor/favoritos/adicionar";
+  static const removeFromFavorites = host + "vendedor/favoritos/remover";
+  static const getFavorites = host + "vendedor/favoritos/buscar/";
+  static const isFavorite = host + "vendedor/favoritos/isFavorite";
 
   // FORMA DE PAGAMENTO
-  static const addPaymentModes = host + "api/formaPagamento/adicionar";
-  static const removePaymentModes = host + "api/formaPagamento/remover";
-  static const getPaymentModesBySeller = host + "api/formaPagamento/buscar/";
-  static const listPaymentModes = host + "api/formaPagamento/buscar";
+  static const addPaymentModes = host + "formaPagamento/adicionar";
+  static const removePaymentModes = host + "formaPagamento/remover";
+  static const getPaymentModesBySeller = host + "formaPagamento/buscar/";
+  static const listPaymentModes = host + "formaPagamento/buscar";
 
   // PRODUTO
   static const searchProdutoPorVendedor =
-      host + "api/produto/buscarPorVendedor";
-  static const searchProductAll = host + "api/produto/buscarTodos";
-  static const registerProduct = host + "api/produto/cadastro";
-  static const updateProduct = host + "api/produto/update/";
-  static const deleteProduct = host + "api/produto/deletar/";
+      host + "produto/buscarPorVendedor";
+  static const searchProductAll = host + "produto/buscarTodos";
+  static const registerProduct = host + "produto/cadastro";
+  static const updateProduct = host + "produto/update/";
+  static const deleteProduct = host + "produto/deletar/";
 
   // RESERVA
-  static const createReservation = host + "api/reserva/create";
-  static const cancelReservation = host + "api/reserva/cancel/";
-  static const confirmReservation = host + "api/reserva/confirm/";
-  static const getReservationByCustomer = host + "api/reserva/getByCustomer/";
-  static const getReservationBySeller = host + "api/reserva/getBySeller/";
+  static const createReservation = host + "reserva/create";
+  static const cancelReservation = host + "reserva/cancel/";
+  static const confirmReservation = host + "reserva/confirm/";
+  static const getReservationByCustomer = host + "reserva/getByCustomer/";
+  static const getReservationBySeller = host + "reserva/getBySeller/";
 
   // AVALIAÇÃO
-  static const insertRating = host + "api/avaliacao/insert";
-  static const getCustomeratings = host + "api/avaliacao/cliente/";
-  static const getSellerRatings = host + "api/avaliacao/vendedor/";
-  static const getProductRatings = host + "api/avaliacao/produto/";
+  static const insertRating = host + "avaliacao/insert";
+  static const getCustomeratings = host + "avaliacao/cliente/";
+  static const getSellerRatings = host + "avaliacao/vendedor/";
+  static const getProductRatings = host + "avaliacao/produto/";
+
+  // RELATÓRIO
+  static const generateReport = host + "relatorios/criar";
 }
